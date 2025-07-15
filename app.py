af9ae4fe7453dce88f70908fd0a4040d18101dc5
dijaykumar <dijaykumar@gmail.com>
2025-07-15 11:51:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import joblib
 from groq import Groq
 
 import os
-os.environ['GROQ_API_KEY'] = os.getenv("groq")
 
 app = Flask(__name__)
 
@@ -37,6 +36,26 @@ def llama_reply():
     )
     return(render_template("llama_reply.html",r=completion.choices[0].message.content))
 
+@app.route("/deepseek",methods=["GET","POST"])
+def deepseek():
+    return(render_template("deepseek.html"))
+
+@app.route("/deepseek_reply",methods=["GET","POST"])
+def deepseek_reply():
+    q = request.form.get("q")
+    # load model
+    client = Groq()
+    completion_ds = client.chat.completions.create(
+        model="deepseek-r1-distill-llama-70b",
+        messages=[
+            {
+                "role": "user",
+                "content": q
+            }
+        ]
+    )
+    return(render_template("deepseek_reply.html",r=completion_ds.choices[0].message.content))
+
 @app.route("/dbs",methods=["GET","POST"])
 def dbs():
     return(render_template("dbs.html"))
@@ -52,4 +71,3 @@ def prediction():
 
 if __name__ == "__main__":
     app.run()
-
